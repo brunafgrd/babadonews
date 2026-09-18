475c4771744dc39f1edb5c44a65e3576ac114b90
Bruna Figueiredo <7v8sz6j454@privaterelay.appleid.com>
2026-09-18 18:01:10 -0300

    Index + css + js
    Primeiros testes da página inicial + css + js + logo+ favicon

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+// =================================
+// BUSCA DE NOTÍCIAS
+// =================================
+
+
+// Seleciona os elementos do formulário
+
+const formularioBusca = document.getElementById("formularioBusca");
+
+const campoPesquisa = document.getElementById("campoPesquisa");
+
+
+// Função responsável pela busca
+
+function realizarBusca(evento) {
+
+    evento.preventDefault();
+
+
+    const termoPesquisa = campoPesquisa.value.trim();
+
+
+    if (termoPesquisa === "") {
+
+        alert("Digite uma notícia para pesquisar.");
+
+        return;
+
+    }
+
+
+    alert("Você pesquisou por: " + termoPesquisa);
+
+}
+
+
+// Evento de envio
+
+formularioBusca.addEventListener("submit", realizarBusca);
